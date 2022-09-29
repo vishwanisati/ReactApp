d73b579bc4c0d7aa714744abb246cad936d026ac
Vishwani Sati <vishwanisati2022@u.northwestern.edu>
2022-09-29 18:51:52 -0500

    initial page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const schedule = {
+  title: "CS Courses for 2018-2019"
+}; 
+
+  const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          CS Course Scheduler <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1> {schedule.title} </h1>
   );
 }
 
