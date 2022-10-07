678a2e011b6962f61a97495d0847a61fb97d9c2f
Vishwani Sati <vishwanisati2022@u.northwestern.edu>
2022-10-07 17:36:47 -0500

    added json fetch

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -5,7 +5,8 @@ const courseList  = ({courses}) => {
     let courseKeys = Object.keys(courses);
     return <div className="course-list"> 
         {courseKeys.map((key, i) =>  
-            <CourseCard title={`${courses[key].term} CS ${courses[key].number}`}
+            <CourseCard key = {i}
+            title={`${courses[key].term} CS ${courses[key].number}`}
                         name={courses[key].title}
                         time={courses[key].meets}
             />
